5c343aaafc87c40e3dda1a8449d1936de3b33496
Rami Awadallah <rami.moh.awadallah@gmail.com>
2019-04-03 11:20:15 +0300

    UP

--- src/views/Helpers/bsForm/image.blade.php
+++ src/views/Helpers/bsForm/image.blade.php
@@ -1,14 +1,13 @@
 
 <?php
     $name = !empty($name) ? $name : '';
-    $path = url('/storage/app');
     $url =  !empty($url) ? $url : 'demo/unknown_image.png' ;
 ?>
 
 
  <div class="form-group">
   <div class="main-img-preview">
-    <img  class="img-fluid img-preview" alt="Responsive image"  src="{{ $path.'/'.$url }}" alt="your image" />
+    <img  class="img-fluid img-preview" alt="Responsive image"  src="{{ Storage::url($url) }}" alt="your image" />
     <!-- <img class="thumbnail img-preview" src="http://farm4.static.flickr.com/3316/3546531954_eef60a3d37.jpg" title="Preview Logo"> -->
   </div>
   <div class="input-group">
