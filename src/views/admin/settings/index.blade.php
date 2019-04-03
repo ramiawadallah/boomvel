85d4642451772d0a1885208f1090524e53277f96
Rami Awadallah <rami.moh.awadallah@gmail.com>
2019-04-03 16:03:57 +0300

    up

--- src/views/admin/settings/index.blade.php
+++ src/views/admin/settings/index.blade.php
@@ -20,46 +20,23 @@
                     $form->text('title',site('title',$lang));
                     $form->text('subtitle',site('subtitle',$lang));
                     $form->text('copyright',site('copyright',$lang));
-                    $form->text('address_jordan',site('address_jordan',$lang));
-                    $form->text('address_sudan',site('address_sudan',$lang));
+                    $form->text('address',site('address',$lang));
                     $form->textarea('desc',site('desc',$lang));
                 }) !!}
 
                 {!! bsForm::text('mainvideo',site('mainvideo')) !!}
 
-                <div class="form-group">
-                    <label class="alert alert-info">Jordan Information</label>
-                </div>
-
-                {!! bsForm::text('email_jordan',site('email_jordan')) !!}
+                {!! bsForm::text('email',site('email')) !!}
                 
-                {!! bsForm::text('phone_jordan',site('phone_jordan')) !!}
+                {!! bsForm::text('phone',site('phone')) !!}
 
-                {!! bsForm::text('fax_jordan',site('fax_jordan')) !!}
+                {!! bsForm::text('fax',site('fax')) !!}
 
-                {!! bsForm::text('pobox_jordan',site('pobox_jordan')) !!}
+                {!! bsForm::text('pobox',site('pobox')) !!}
 
-                {!! bsForm::text('map_jordan',site('map_jordan')) !!}
+                {!! bsForm::text('map',site('map')) !!}
 
-                <hr>
-
-                <div class="form-group">
-                    <label class="alert alert-info">Sudan Information</label>
-                </div>
-
-                {!! bsForm::text('email_sudan',site('email_sudan')) !!}
-                
-                {!! bsForm::text('phone_sudan',site('phone_sudan')) !!}
-
-                {!! bsForm::text('fax_sudan',site('fax_sudan')) !!}
-
-                {!! bsForm::text('pobox_sudan',site('pobox_sudan')) !!}
-
-                {!! bsForm::text('map_sudan',site('map_sudan')) !!}
-
-
-
-                {!! bsForm::text('keywords',site('keywords')) !!}
+                {!! bsForm::tage('keywords',site('keywords')) !!}
 
                 {!! bsForm::text('facebook',site('facebook')) !!}
 
@@ -81,9 +58,7 @@
                 <h3 class="block-title">{{ trans('lang.options') }}</h3>
             </div>
             <div class="block-content block-content-full">
-                {!! bsForm::imageone('logo',$settings->logo) !!}
-                {!! bsForm::imagetwo('image_one',$settings->image_one) !!}
-                {!! bsForm::imagethree('image_two',$settings->image_two) !!}
+                {!! bsForm::image('logo',$settings->logo) !!}
                 <hr>
                 {!! bsForm::radio('maintenance',[
                     'open'=> trans('lang.maintenance-open'),
